33a0c8186a849ab7d7e2688739842f356420e5d7
Dorian Stoll <dorian@tmsp.io>
2017-02-07 22:25:10 +0100

    Add Eto.Forms Project

---
+++ HVH.Client.Linux/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * HVH.Client - User interface for the HVH.* infrastructure
+ * Copyright (c) Dorian Stoll 2017
+ * Licensed under the terms of the MIT License
+ */
+
+ using System;
+using Eto;
+using Eto.Forms;
+
+namespace HVH.Client.Linux
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Platforms.Gtk3).Run(new MainForm());
+        }
+    }
+}
---
+++ HVH.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("HVH.Client")]
+[assembly: AssemblyDescription("User interface for using and administrating the HVH.* infrastructure")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Dorian Stoll 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
+
